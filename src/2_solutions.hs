84948c00c471cb2485c45b070b85c12c7983e116
macksimiljan <macksimiljan@users.noreply.github.com>
2017-10-15 09:39:08 +0200

    implemented wordLength and wordConcat

--- src/2_solutions.hs
+++ src/2_solutions.hs
@@ -7,9 +7,19 @@ instance Show Word_1 where
     show EmptyWord    = "#"
     show (Word_1 s w) = show(s) ++ ":" ++ show(w)
 
+-- | Laenge eines Wortes
+wordLength :: Word_1 -> Int
+wordLength EmptyWord    = 0
+wordLength (Word_1 s w) = 1 + wordLength w
+
+-- | Verkette zwei Woerter
+wordConcat :: Word_1 -> Word_1 -> Word_1
+wordConcat EmptyWord v      = v
+wordConcat (Word_1 s1 w1) v = Word_1 s1 (wordConcat w1 v)
+
 -- | Def 2: Wort
-data Word_2 = EmptyWord | Word_2 Word_2 Symbol
+data Word_2 = EmptyWord_2 | Word_2 Word_2 Symbol
 instance Show Word_2 where
-    show EmptyWord    = "#"
-    show (Word_2 w s) = show(w) ++ ":" ++ show(s)
+    show EmptyWord_2   = "#"
+    show (Word_2 w s)  = show(w) ++ ":" ++ show(s)
 
